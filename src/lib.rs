ccacd1ef897f9e7272be7d373747667d97e482f0
mechiru <46036584+mechiru@users.noreply.github.com>
2020-11-10 12:08:31 +0900

    Use RawValue
    * Map<String, Value>
    
    bench:
    test bench_deserialize ... bench:       5,712 ns/iter (+/- 462)
    test bench_deserialize ... bench:       5,717 ns/iter (+/- 529)
    test bench_deserialize ... bench:       5,708 ns/iter (+/- 497)
    test bench_deserialize ... bench:       5,753 ns/iter (+/- 737)
    
    allocate:
       57248 bytes allocated / 3481600 bytes resident
       93664 bytes allocated / 3522560 bytes resident
    
    * RawValue
    
    bench:
    test bench_deserialize ... bench:         613 ns/iter (+/- 64)
    test bench_deserialize ... bench:         611 ns/iter (+/- 84)
    test bench_deserialize ... bench:         617 ns/iter (+/- 84)
    test bench_deserialize ... bench:         611 ns/iter (+/- 63)
    test bench_deserialize ... bench:         615 ns/iter (+/- 64)
    
    allocate:
       57248 bytes allocated / 3481600 bytes resident
       71584 bytes allocated / 3497984 bytes resident

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,56 @@
 //! This library provides an extension object for [serde_json](crates.io/crates/serde_json) crate.
 
-use serde::{de, ser, Deserialize, Serialize};
-use serde_json::{Map, Value};
+use serde::{de, ser};
+use serde_json::value::RawValue;
 
-use std::convert::{From, Into};
+/// Represents a custom JSON object.
+#[derive(Debug, Clone)]
+pub struct Ext<'a>(&'a RawValue);
 
-/// This object is a placeholder for a custom json.
-#[derive(Serialize, Deserialize, Default, Debug, PartialEq, Clone)]
-pub struct Ext(Map<String, Value>);
-
-impl Ext {
-    /// Convert the value to an `Ext` object.
-    pub fn try_from<T>(value: T) -> serde_json::Result<Self>
+impl<'a> Ext<'a> {
+    /// Convert a `&RawValue` to an `Ext` object.
+    pub fn try_from<'b>(raw: &'b RawValue) -> serde_json::Result<Ext<'a>>
     where
-        T: Serialize,
+        'b: 'a,
     {
-        match serde_json::to_value(value)? {
-            Value::Object(m) => Ok(Self(m)),
-            _ => Err(ser::Error::custom(
-                "must pass a value that can be converted to `serde_json::Map`",
-            )),
-        }
+        check_raw(raw).map(Self).map_err(ser::Error::custom)
     }
 
-    /// Converts to a specified type.
-    pub fn try_into<T>(self) -> serde_json::Result<T>
-    where
-        T: de::DeserializeOwned,
-    {
-        serde_json::from_value(Value::Object(self.0))
+    /// Access the JSON text.
+    pub fn get(&'a self) -> &'a str {
+        self.0.get()
     }
 }
 
-impl From<Map<String, Value>> for Ext {
-    fn from(value: Map<String, Value>) -> Self {
-        Self(value)
+impl<'a> PartialEq for Ext<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.get() == other.0.get()
     }
 }
 
-impl Into<Map<String, Value>> for Ext {
-    fn into(self) -> Map<String, Value> {
-        self.0
+impl<'a> ser::Serialize for Ext<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
+
+impl<'de: 'a, 'a> de::Deserialize<'de> for Ext<'a> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let raw: &'a RawValue = de::Deserialize::deserialize(deserializer)?;
+        check_raw(raw).map(Self).map_err(de::Error::custom)
+    }
+}
+
+fn check_raw(raw: &RawValue) -> Result<&RawValue, &'static str> {
+    match raw.get().as_bytes().get(0) {
+        Some(b'{') | Some(b'n') => Ok(raw),
+        _ => Err("invalid value: expected null or object"),
     }
 }
 
@@ -48,79 +58,68 @@ impl Into<Map<String, Value>> for Ext {
 mod test {
     use super::*;
 
-    #[test]
-    fn from_and_into() {
-        let m = Map::new();
-        let e: Ext = m.into();
-        let _: Map<String, Value> = e.into();
+    use serde::{Deserialize, Serialize};
+    use serde_json::{from_str, value::to_raw_value, Map, Value};
+
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    struct Object<'a> {
+        #[serde(borrow)]
+        ext: Ext<'a>,
+    }
+
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    struct Extension<'a> {
+        f1: &'a str,
+        f2: i32,
     }
 
     #[test]
-    fn try_from_and_try_into() -> serde_json::Result<()> {
-        assert!(Ext::try_from(serde_json::json!(true)).is_err());
-
-        let ext = Ext::try_from(serde_json::json!({"a": 1}))?;
-        assert_eq!(
-            ext,
-            Ext({
-                let mut m = Map::new();
-                m.insert("a".into(), Value::Number(1.into()));
-                m
-            }),
-        );
-        assert_eq!(ext.try_into::<Value>()?, serde_json::json!({"a":  1}));
-
-        #[derive(Serialize, Deserialize, Debug, PartialEq)]
-        struct Object {
-            f1: bool,
-            f2: i32,
-        }
-        let ext = Ext::try_from(Object { f1: true, f2: 123 })?;
-        assert_eq!(
-            ext,
-            Ext({
-                let mut m = Map::new();
-                m.insert("f1".into(), Value::Bool(true));
-                m.insert("f2".into(), Value::Number(123.into()));
-                m
-            })
-        );
-        assert_eq!(ext.try_into::<Object>()?, Object { f1: true, f2: 123 });
+    fn try_from_and_get() -> serde_json::Result<()> {
+        let object = Value::Object({
+            let mut m = Map::new();
+            m.insert("f1".into(), Value::String("abc".into()));
+            m.insert("f2".into(), Value::Number(123.into()));
+            m
+        });
+        let raw = to_raw_value(&object)?;
+        let ext = Ext::try_from(raw.as_ref())?;
+        let ext2 = from_str::<Extension>(ext.get())?;
+        assert_eq!(ext2, Extension { f1: "abc", f2: 123 });
 
         Ok(())
     }
 
     #[test]
     fn json() -> serde_json::Result<()> {
-        let json = r#"{"a":1}"#;
-        let ext: Ext = serde_json::from_str(json)?;
-        assert_eq!(
-            ext,
-            Ext({
-                let mut m = Map::new();
-                m.insert("a".into(), Value::Number(1.into()));
-                m
-            })
-        );
-        assert_eq!(serde_json::to_string(&ext)?, json);
+        assert!(from_str::<Object>(r#"{"ext":null}"#).is_ok());
+        assert!(from_str::<Object>(r#"{"ext":true}"#).is_err());
+        assert!(from_str::<Object>(r#"{"ext":1}"#).is_err());
+        assert!(from_str::<Object>(r#"{"ext":"1"}"#).is_err());
+        assert!(from_str::<Object>(r#"{"ext":[1]}"#).is_err());
+        assert!(from_str::<Object>(r#"{"ext":{}}"#).is_ok());
 
-        #[derive(Serialize, Deserialize, Debug, PartialEq)]
-        struct Object {
-            ext: Ext,
-        }
-        let json = r#"{"ext":{"a":true}}"#;
-        let obj: Object = serde_json::from_str(json)?;
+        let s1 = r#"{"ext": 	
+null}"#;
+        let o1 = from_str::<Object>(s1)?;
         assert_eq!(
-            obj,
+            o1,
             Object {
-                ext: Ext({
-                    let mut m = Map::new();
-                    m.insert("a".into(), Value::Bool(true));
-                    m
-                })
+                ext: Ext::try_from(to_raw_value(&Value::Null)?.as_ref())?,
             }
         );
-        assert_eq!(serde_json::to_string(&obj)?, json);
+        assert_eq!(serde_json::to_string(&o1)?, r#"{"ext":null}"#);
+
+        let s2 = r#"{"ext": 	
+{}
+ 	}"#;
+        let o2 = serde_json::from_str::<Object>(s2)?;
+        assert_eq!(
+            o2,
+            Object {
+                ext: Ext::try_from(to_raw_value(&Value::Object(Default::default()))?.as_ref())?,
+            }
+        );
+        assert_eq!(serde_json::to_string(&o2)?, r#"{"ext":{}}"#);
 
         Ok(())
     }
